6490a334286e6626f4e8faae2bb72755332aa49e
ninoprek <ninoprekratic@gmail.com>
2018-04-24 16:28:38 +0200

    Saving logic for user profile

--- app/src/test/java/jumanji/sda/com/jumanji/UserProfileRepositoryTest.kt
+++ app/src/test/java/jumanji/sda/com/jumanji/UserProfileRepositoryTest.kt
@@ -1,10 +1,13 @@
 package jumanji.sda.com.jumanji
 
+import android.app.Application
+import android.content.Context
+import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-
-import org.junit.Assert.*
 
 class UserProfileRepositoryTest {
 
@@ -16,11 +19,15 @@ class UserProfileRepositoryTest {
     fun tearDown() {
     }
 
-    @Test
-    fun getTestUser() {
-    }
 
     @Test
     fun saveToDatabese() {
+
+        val userProfileRepository: UserProfileRepository = UserProfileRepository()
+
+        val testUserProfile: UserProfile = UserProfile("Aaron", "aaron@gmai.com", "www.picture.com")
+
+        userProfileRepository.storeToDatabase(testUserProfile)
+
     }
 }
