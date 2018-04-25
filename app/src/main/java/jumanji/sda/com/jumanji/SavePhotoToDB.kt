b3a885df724b1f9609d376655498fbfb0be5bf93
Debby Jane Azarcon <dja.difuntorum@gmail.com>
2018-04-25 15:27:33 +0200

    Added save to photo functionality

---
+++ app/src/main/java/jumanji/sda/com/jumanji/SavePhotoToDB.kt
@@ -0,0 +1,30 @@
+package jumanji.sda.com.jumanji
+
+import android.net.Uri
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.storage.UploadTask
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.StorageReference
+import java.io.File
+
+
+class SavePhotoToDB {
+
+/*    fun savePhoto() {
+
+        val file = Uri.fromFile(File("path/to/images/rivers.jpg"))
+        val riversRef = storageRef.child("images/rivers.jpg")
+
+        riversRef.putFile(file)
+                .addOnSuccessListener(OnSuccessListener<UploadTask.TaskSnapshot> { taskSnapshot ->
+                    // Get a URL to the uploaded content
+                    val downloadUrl = taskSnapshot.downloadUrl
+                })
+                .addOnFailureListener(OnFailureListener {
+                    // Handle unsuccessful uploads
+                    // ...
+                })
+    }*/
+
+}
