40cafb24acb0b9901c4829425a7a1cfb93107082
aaronsum1102 <sumsx03@gmail.com>
2018-05-02 11:05:45 +0200

    Move starting of camera and image picker intent to Map Fragment

--- app/src/main/java/jumanji/sda/com/jumanji/ProgramActivity.kt
+++ app/src/main/java/jumanji/sda/com/jumanji/ProgramActivity.kt
@@ -1,25 +1,14 @@
 package jumanji.sda.com.jumanji
 
-import android.content.DialogInterface
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.provider.MediaStore
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.fragment_home_page.*
 
-class ProgramActivity : AppCompatActivity(), PhotoListener {
-    companion object {
-        const val REQUEST_CAMERA = 100
-        const val SELECT_FILE = 200
-    }
-
-    var userChoosenTask: String = ""
+class ProgramActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_home_page)
@@ -46,52 +35,4 @@ class ProgramActivity : AppCompatActivity(), PhotoListener {
 
         override fun getCount(): Int = NO_OF_TABS
     }
-
-    override fun selectImage() {
-        val items = arrayOf<CharSequence>("Take Photo", "Choose from Library", "Cancel")
-        val builder = AlertDialog.Builder(this@ProgramActivity)
-        builder.setTitle("Add Photo!")
-        builder.setItems(items, DialogInterface.OnClickListener { dialog, item ->
-            val result = Utility.checkPermission(this@ProgramActivity)
-            if (items[item] == "Take Photo") {
-                userChoosenTask = "Take Photo"
-                if (result)
-                    cameraIntent()
-            } else if (items[item] == "Choose from Library") {
-                userChoosenTask = "Choose from Library"
-                if (result)
-                    galleryIntent()
-            } else if (items[item] == "Cancel") {
-                dialog.dismiss()
-            }
-        })
-        builder.show()
-    }
-
-    private fun cameraIntent() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(intent, ProgramActivity.REQUEST_CAMERA)
-    }
-
-    private fun galleryIntent() {
-        val intent = Intent()
-        intent.type = "image/*"
-        intent.action = Intent.ACTION_GET_CONTENT//
-        startActivityForResult(Intent.createChooser(intent, "Select File"), ProgramActivity.SELECT_FILE)
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE -> if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                if (userChoosenTask.equals("Take Photo"))
-                    cameraIntent()
-                else if (userChoosenTask.equals("Choose from Library"))
-                    galleryIntent()
-            } else {
-                //code for deny
-            }
-        }
-    }
-
 }
